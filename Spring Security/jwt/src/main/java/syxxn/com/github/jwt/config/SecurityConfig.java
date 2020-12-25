7f31fca4701c35de92d93a2d41cc368cc6070dfe
이승윤 <yyuunn17@naver.com>
2020-12-25 20:16:55 +0900

    delete: import annotation

--- Spring Security/jwt/src/main/java/syxxn/com/github/jwt/config/SecurityConfig.java
+++ Spring Security/jwt/src/main/java/syxxn/com/github/jwt/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package syxxn.com.github.jwt.config;
 
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
