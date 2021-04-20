e2ffa48a913a50a668769c5516b29664b5738c70
이승윤 <yyuunn17@naver.com>
2021-04-20 22:38:09 +0900

    [ADD] 프로젝트 구조 추가

---
+++ Springboot Example/src/test/java/com/example/sprinboottest/SprinbootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprinboottest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinbootTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
